30a14c3f537378111a67558cdb9d91d4eeedbd03
Isaque-Ribeiro <133152404+Isaque-Ribeiro@users.noreply.github.com>
2024-03-30 01:37:54 -0300

    radixsort.h

---
+++ RadixSort/radixsort.h
@@ -0,0 +1,12 @@
+// functions
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void fLower(char**, int);  //convert uppercases
+int findBiger(char**, int);
+char findnKey(char, char*);
+char** CountingSort(char**, char*, int, int);
+char** RadixSort(char**, char*, int, int);
