8181b3a56e3b95674f6acde97008a5cdf266b879
Isaque-Ribeiro <133152404+Isaque-Ribeiro@users.noreply.github.com>
2024-03-30 01:22:07 -0300

    quick.h

---
+++ QuickSort/quick.h
@@ -0,0 +1,12 @@
+// functions catalog
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void imprimeLista(int*,int);
+void swap(int*,int,int);
+void CrashByMedian(int*,int,int);
+int EndQuickSort(int*,int,int);
+int countLR(int*,int,int,int);
+int MedianQuickSort(int*,int,int);
